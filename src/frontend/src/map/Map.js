2a1cf61b3af370904decda185cf416ae45ccb38f
Mare0804 <63041075+Mare0804@users.noreply.github.com>
2021-09-06 08:48:31 +0200

    Update Map.js

--- src/frontend/src/map/Map.js
+++ src/frontend/src/map/Map.js
@@ -25,5 +25,5 @@ export class MapContainer extends Component {
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyDaTZ91-ZbJkUyUCKGIhsYQ0JfYZambLBk'
+    apiKey: 'insert your key here'
 })(MapContainer);
